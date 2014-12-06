28589c273dbe348144c61389d3dd162c06a2d3e2
renzongxian <narouto72@gmail.com>
2014-12-06 20:09:49 +0800

    修改问题内容

--- 0002.py
+++ 0002.py
@@ -5,9 +5,7 @@
 # Python 2.7, MySQL-python does not currently support Python 3
 
 """
-
-做为 Apple Store App 独立开发者，你要搞限时促销，为你的应用生成激活码（或者优惠券），
-使用 Python 如何生成 200 个激活码（或者优惠券）？
+将 0001 题生成的 200 个激活码（或者优惠券）保存到 MySQL 关系型数据库中。
 
 """
 
