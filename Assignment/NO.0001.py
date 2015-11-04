a7577cd881a3f04ad0e434f18a6e34981e33542f
Fython <fang.jia.hui123@gmail.com>
2015-11-04 20:39:07 +0800

    NO.0001 complete

--- Assignment/NO.0001.py
+++ Assignment/NO.0001.py
@@ -3,3 +3,32 @@
 
 
 print 'NO.001 头像加数字'
+
+import Image, ImageDraw, ImageFont, random
+
+#导入图片
+im = Image.open('portrait2.jpeg')
+
+w, h = im.size
+
+print 'width = %d \nheight = %d' % (w, h)
+print '------------------>'
+
+#创建draw对象
+draw = ImageDraw.Draw(im)
+
+#创建font对象
+font = ImageFont.truetype(
+'/usr/share/fonts/truetype/freefont/FreeSansOblique.ttf', 36)
+
+#字体颜色 、内容
+def col():
+	return (255, 0, 0)
+
+text = str(random.randint(0, 9))
+
+#写字
+draw.text((165, 5), text , font = font, fill = col())
+
+#save
+im.save('tagportrait.jpg', 'jpeg')
