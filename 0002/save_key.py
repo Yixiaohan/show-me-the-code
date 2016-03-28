5139511a87f75d8dcee0cd81b42c2c109a1662d5
wangxiaoxian <634897019@qq.com>
2016-03-28 08:44:01 -0700

    finish 0002

--- 0002/save_key.py
+++ 0002/save_key.py
@@ -22,7 +22,7 @@ def key_num(num,result=None):
     if result is None:
         result = []
     for i in range(num):
-        result.append(key_gen())
+        result.append(str(key_gen()))
     return result
 
 class mysql_init(object):
@@ -56,52 +56,56 @@ class mysql_init(object):
     
 def process():
     dbconn.connect()
-    DropTable()
-    CreateTable()
-    InsertDatas()
-    QueryData()
+    conn=dbconn.cursor()
+    DropTable(conn)
+    CreateTable(conn)
+    InsertDatas(conn)
+    QueryData(conn)
     dbconn.close()
 
-def execute(sql):
-    '''执行sql'''
-    conn=dbconn.cursor()
-    conn.execute(sql)
+#def execute(sql):
+#    '''执行sql'''
+#    conn=dbconn.cursor()
+#    conn.execute(sql)
 
 #def executemany(sql, tmp):
 #    '''插入多条数据'''
 #    conn=dbconn.cursor()
 #    conn.executemany(sql,tmp)
 
-def query(sql):
+def query(sql,conn):
     '''查询sql'''
-    conn=dbconn.cursor()
+    #conn=dbconn.cursor()
     conn.execute(sql)
-    rows = conn.fetchmany(10)
+    rows = conn.fetchall()
     return rows
 
-def DropTable():
-    conn=dbconn.cursor()
+def DropTable(conn):
+    #conn=dbconn.cursor()
     conn.execute("DROP TABLE IF EXISTS `user_key`")
      
-def CreateTable():
-    conn=dbconn.cursor()
-    sql_create =''' CREATE TABLE `user_key` (`ID` int(5) NOT NULL auto_increment,`key` varchar(100) NOT NULL,PRIMARY KEY (`ID`))'''
+def CreateTable(conn):
+    #conn=dbconn.cursor()
+    sql_create =''' CREATE TABLE `user_key` (`key` varchar(50) NOT NULL)'''
     conn.execute(sql_create)
 
-def InsertDatas():
-    conn=dbconn.cursor()
-    insert_sql = "insert into user_key(key) values(%s)"
+def InsertDatas(conn):
+    #conn=dbconn.cursor()
+    #insert_sql = "insert into user_key values(%s)"
+    insert_sql = "INSERT INTO user_key VALUES (%(value)s)"
     key_list = key_num(KEY_ALL)
-    print len(key_list)
-    conn.executemany(insert_sql,key_list)
+    #print len(key_list)
+    #conn.executemany(insert_sql,str(key_listi))
+    #conn.executemany("INSERT INTO user_key VALUES (%(value)s)",[dict(value=v) for v in key_list])
+    conn.executemany(insert_sql,[dict(value=v) for v in key_list])
 
 def DeleteData():
     del_sql = "delete from user_key where id=2"
     execute(del_sql)
 
-def QueryData():
+def QueryData(conn):
     sql = "select * from user_key"
-    rows = query(sql)
+    rows = query(sql,conn)
     printResult(rows)
 
 def printResult(rows):
