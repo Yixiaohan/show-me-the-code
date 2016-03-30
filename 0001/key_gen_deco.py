a3aaea8dee8dd065c4af759d925967e1e31b37d9
wangxx <wangxiaoxian@ebupt.com>
2016-03-30 18:58:17 +0800

    pep8 style

--- 0001/key_gen_deco.py
+++ 0001/key_gen_deco.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 
-"""
-第 0001 题：做为 Apple Store App 独立开发者，你要搞限时促销，为你的应用生成激活码（或者优惠券），使用 Python 如何生成 200 个激活码（或者优惠券）？
-"""
+# 第 0001 题：做为 Apple Store App 独立开发者，你要搞限时促销，为你的应用生成激活码（或者优惠券），
+# 使用 Python 如何生成 200 个激活码（或者优惠券）？
 
 import string
 import random
@@ -11,12 +10,15 @@ import random
 KEY_LEN = 20
 KEY_ALL = 200
 
+
 def base_str():
     return (string.letters+string.digits)
 
-def key_gen():  
+
+def key_gen():
     keylist = [random.choice(base_str()) for i in range(KEY_LEN)]
     return ("".join(keylist))
+
 
 def print_key(func):
     def _print_key(num):
@@ -24,14 +26,16 @@ def print_key(func):
             print i
     return _print_key
 
+
 @print_key
-def key_num(num,result=None):
+def key_num(num, result=None):
     if result is None:
         result = []
     for i in range(num):
         result.append(key_gen())
     return result
 
+
 if __name__ == "__main__":
-    #print_key(KEY_ALL)
+    # print_key(KEY_ALL)
     key_num(KEY_ALL)
