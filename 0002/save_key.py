77773eeaf9324956e027dd3533f1ebdafa71cf01
wangxx <wangxiaoxian@ebupt.com>
2016-03-30 19:10:01 +0800

    pep8 style

--- 0002/save_key.py
+++ 0002/save_key.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 
-"""
-第 0002 题：将 0001 题生成的 200 个激活码（或者优惠券）保存到 MySQL 关系型数据库中。
-"""
+# 第 0002 题：将 0001 题生成的 200 个激活码（或者优惠券）保存到 MySQL 关系型数据库中。
+
 import MySQLdb
 import string
 import random
@@ -11,40 +10,44 @@ import random
 KEY_LEN = 20
 KEY_ALL = 200
 
+
 def base_str():
-    return (string.letters+string.digits)
+    return (string.letters + string.digits)
+
 
 def key_gen():
     keylist = [random.choice(base_str()) for i in range(KEY_LEN)]
     return ("".join(keylist))
 
-def key_num(num,result=None):
+
+def key_num(num, result=None):
     if result is None:
         result = []
     for i in range(num):
         result.append(str(key_gen()))
     return result
 
+
 class mysql_init(object):
 
-    def __init__(self,conn):
+    def __init__(self, conn):
         self.conn = None
 
-    #connect to mysql
+    # connect to mysql
     def connect(self):
         self.conn = MySQLdb.connect(
-	    host = "localhost",
-            port = 3306,
-            user = "root",
-            passwd = "123456",
-            db = "test",
-            charset = "utf8"    	
-	)
+            host="localhost",
+            port=3306,
+            user="root",
+            passwd="123456",
+            db="test",
+            charset="utf8"
+        )
 
     def cursor(self):
         try:
             return self.conn.cursor()
-        except (AttributeError,MySQLdb.OperationalError):
+        except (AttributeError, MySQLdb.OperationalError):
             self.connect()
             return self.conn.cursor()
 
@@ -53,60 +56,69 @@ class mysql_init(object):
 
     def close(self):
         return self.conn.close()
-    
+
+
 def process():
     dbconn.connect()
-    conn=dbconn.cursor()
+    conn = dbconn.cursor()
     DropTable(conn)
     CreateTable(conn)
     InsertDatas(conn)
     QueryData(conn)
     dbconn.close()
 
-#def execute(sql):
-#    '''执行sql'''
-#    conn=dbconn.cursor()
-#    conn.execute(sql)
+# def execute(sql):
+#     '''执行sql'''
+#     conn=dbconn.cursor()
+#     conn.execute(sql)
 
-#def executemany(sql, tmp):
-#    '''插入多条数据'''
-#    conn=dbconn.cursor()
-#    conn.executemany(sql,tmp)
+# def executemany(sql, tmp):
+#     '''插入多条数据'''
+#     conn=dbconn.cursor()
+#     conn.executemany(sql,tmp)
 
-def query(sql,conn):
+
+def query(sql, conn):
     '''查询sql'''
-    #conn=dbconn.cursor()
+    # conn=dbconn.cursor()
     conn.execute(sql)
     rows = conn.fetchall()
     return rows
 
+
 def DropTable(conn):
-    #conn=dbconn.cursor()
+    # conn=dbconn.cursor()
     conn.execute("DROP TABLE IF EXISTS `user_key`")
-     
+
+
 def CreateTable(conn):
-    #conn=dbconn.cursor()
-    sql_create =''' CREATE TABLE `user_key` (`key` varchar(50) NOT NULL)'''
+    # conn=dbconn.cursor()
+    sql_create = ''' CREATE TABLE `user_key` (`key` varchar(50) NOT NULL)'''
     conn.execute(sql_create)
 
+
 def InsertDatas(conn):
-    #conn=dbconn.cursor()
-    #insert_sql = "insert into user_key values(%s)"
+    # conn=dbconn.cursor()
+    # insert_sql = "insert into user_key values(%s)"
     insert_sql = "INSERT INTO user_key VALUES (%(value)s)"
     key_list = key_num(KEY_ALL)
-    #print len(key_list)
-    #conn.executemany(insert_sql,str(key_listi))
-    #conn.executemany("INSERT INTO user_key VALUES (%(value)s)",[dict(value=v) for v in key_list])
-    conn.executemany(insert_sql,[dict(value=v) for v in key_list])
+    # print len(key_list)
+    # conn.executemany(insert_sql,str(key_listi))
+    # conn.executemany("INSERT INTO user_key VALUES (%(value)s)",
+    #                   [dict(value=v) for v in key_list])
+    conn.executemany(insert_sql, [dict(value=v) for v in key_list])
+
 
 def DeleteData():
     del_sql = "delete from user_key where id=2"
     execute(del_sql)
 
+
 def QueryData(conn):
     sql = "select * from user_key"
-    rows = query(sql,conn)
+    rows = query(sql, conn)
     printResult(rows)
+
 
 def printResult(rows):
     if rows is None:
@@ -115,5 +127,5 @@ def printResult(rows):
         print row
 
 if __name__ == "__main__":
-   dbconn = mysql_init(None) 
-   process()
+    dbconn = mysql_init(None)
+    process()
--- 0006/key_word.py
+++ 0006/key_word.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 
-"""
-第 0006 题：你有一个目录，放了你一个月的日记，都是 txt，为了避免分词的问题，假设内容都是英文，请统计出你认为每篇日记最重要的词。
-"""
+# 第 0006 题：你有一个目录，放了你一个月的日记，都是 txt，为了避免分词的问题，假设内容都是英文，请统计出你认为每篇日记最重要的词。
 
 import glob
 from collections import Counter
 import re
 
+
 def list_txt():
     return glob.glob("*.txt")
 
+
 def wc(filename):
     datalist = []
-    with open(filename,'r') as f:
+    with open(filename, 'r') as f:
         for line in f:
-             content = re.sub("\"|,|\.","",line)
-             datalist.extend(content.strip().split(' '))
-    #print datalist
+            content = re.sub("\"|,|\.", "", line)
+            datalist.extend(content.strip().split(' '))
+    # print datalist
     return Counter(datalist).most_common(1)
+
 
 def most_comm():
     for txt in list_txt():
-       print wc(txt)
+        print wc(txt)
 
 if __name__ == "__main__":
-    #most_comm()
-    print map(wc,list_txt())
+    # most_comm()
+    print map(wc, list_txt())
