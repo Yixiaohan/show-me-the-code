ca9c2cc08a2786646033981462698fab9375a12f
YeomanYe <ming13821007683@gmail.com>
2017-02-03 21:13:51 +0800

    update py format

--- 0000/add-num.py
+++ 0000/add-num.py
@@ -7,8 +7,9 @@ def add_num(img):
     font = ImageFont.truetype('C:/windows/fonts/Arial.ttf', size=40)
     color = "#123456"
     width, height = img.size
-    draw.text((width-40,0), str(randint(0,99)), font=font, fill=color)
-    img.save("result.jpg",'jpeg')
+    draw.text((width - 40, 0), str(randint(0, 99)), font=font, fill=color)
+    img.save("result.jpg", 'jpeg')
+
 
 if __name__ == '__main__':
     image = Image.open('image.jpg')
